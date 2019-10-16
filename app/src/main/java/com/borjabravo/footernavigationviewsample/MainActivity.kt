ba861633d708aae0fb888c368cdc4d4908461b99
Jiahao Liu <jiahao.liu@dnata.com>
2019-10-16 11:11:43 +0400

    Using data binding for the layouts

--- app/src/main/java/com/borjabravo/footernavigationviewsample/MainActivity.kt
+++ app/src/main/java/com/borjabravo/footernavigationviewsample/MainActivity.kt
@@ -5,32 +5,34 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import com.borjabravo.footernavigationview.FooterNavigationView
+import com.borjabravo.footernavigationviewsample.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private var drawerLayout: DrawerLayout? = null
-    private var footerNavigationView: FooterNavigationView? = null
-
-    internal var optionSelected = R.id.menu_add
+    private var activityMainBinding: ActivityMainBinding? = null
+    private var optionSelected = R.id.menu_add
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        drawerLayout = findViewById(R.id.drawerLayout)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        toolbar.setNavigationIcon(R.drawable.ic_menu)
-        toolbar.setNavigationOnClickListener { drawerLayout?.openDrawer(GravityCompat.START) }
-        footerNavigationView = findViewById(R.id.footer_navigation_view)
-        footerNavigationView?.setCheckedItem(optionSelected)
-        footerNavigationView?.navigationView?.menu?.performIdentifierAction(optionSelected, 0)
-        footerNavigationView?.setNavigationListener(this)
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Set up toolbar
+        activityMainBinding?.toolbar?.setNavigationIcon(R.drawable.ic_menu)
+        activityMainBinding?.toolbar?.setNavigationOnClickListener { activityMainBinding?.drawerLayout?.openDrawer(GravityCompat.START) }
+
+        // Set up footer navigation view
+        activityMainBinding?.footerNavigationView?.setCheckedItem(optionSelected)
+        activityMainBinding?.footerNavigationView?.navigationView?.menu?.performIdentifierAction(optionSelected, 0)
+        activityMainBinding?.footerNavigationView?.setNavigationListener(this)
     }
 
     override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
-        drawerLayout?.closeDrawers()
+        activityMainBinding?.drawerLayout?.closeDrawers()
         optionSelected = menuItem.itemId
         return true
     }
